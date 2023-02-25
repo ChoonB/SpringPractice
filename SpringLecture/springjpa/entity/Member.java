78b11c29fa12bbf73ef38b88a99b9f9b1119fd1c
ChoonB <shgkdc@gmail.com>
2023-02-25 11:06:54 +0900

    엔티티 객체 생성

--- SpringLecture/springjpa/entity/Member.java
+++ SpringLecture/springjpa/entity/Member.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -15,5 +17,10 @@ public class Member {
     @Column(nullable = false)
     private String memberName;
 
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
+    private List<Orders> orders = new ArrayList<>();
 
+    public Member(String memberName) {
+        this.memberName = memberName;
+    }
 }
