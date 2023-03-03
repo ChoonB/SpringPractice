ee76a22cb94ef9ad18b1011eb5b1ad79b1861406
ChoonB <shgkdc@gmail.com>
2023-03-03 19:53:07 +0900

    리팩토링 완성 + 스케쥴러 완성

--- SpringLecture/myselectshop/java/com/sparta/myselectshop/controller/ProductController.java
+++ SpringLecture/myselectshop/java/com/sparta/myselectshop/controller/ProductController.java
@@ -4,38 +4,35 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController() {
-        this.productService =  new ProductService();
-    }
-
     // 관심 상품 등록하기
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
         // 응답 보내기
         return productService.createProduct(requestDto);
     }
 
     // 관심 상품 조회하기
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts() throws SQLException {
+    public List<ProductResponseDto> getProducts() {
         // 응답 보내기
         return productService.getProducts();
     }
 
     // 관심 상품 최저가 등록하기
     @PutMapping("/products/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         // 응답 보내기 (업데이트된 상품 id)
         return productService.updateProduct(id, requestDto);
     }
