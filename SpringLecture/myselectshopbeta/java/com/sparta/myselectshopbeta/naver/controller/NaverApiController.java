cdd14488300ad71d6fb45df843c27a6797b3b904
ChoonB <shgkdc@gmail.com>
2023-03-03 16:34:12 +0900

    myselectshopbeta naver 패키지

---
+++ SpringLecture/myselectshopbeta/java/com/sparta/myselectshopbeta/naver/controller/NaverApiController.java
@@ -0,0 +1,22 @@
+package com.sparta.myselectshopbeta.naver.controller;
+
+
+import com.sparta.myselectshopbeta.naver.dto.ItemDto;
+import com.sparta.myselectshopbeta.naver.service.NaverApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class NaverApiController {
+
+    private final NaverApiService naverApiService;
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam String query)  {
+        return naverApiService.searchItems(query);
+    }
+}
