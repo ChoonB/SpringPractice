dde9fc204c50bd86f28406cb455432c2cb59b0bb
ChoonB <shgkdc@gmail.com>
2023-03-10 14:07:54 +0900

    springsecurity UserDetailsImpl

---
+++ SpringLecture/springsecurity/main/java/com/sparta/springsecurity/security/UserDetailsImpl.java
@@ -0,0 +1,71 @@
+package com.sparta.springsecurity.security;
+
+
+import com.sparta.springsecurity.entity.User;
+import com.sparta.springsecurity.entity.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+    private final String username;
+    private final String password;
+
+    public UserDetailsImpl(User user, String username, String password) {
+        this.user = user;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    // 권한을 가져오는 부분. User의 권한을 가져와 String 값으로 만들고 추상화해서 사용.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
