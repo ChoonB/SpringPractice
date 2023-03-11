0f27c0e0427f42bfa0ba59109406954b94bde798
ChoonB <shgkdc@gmail.com>
2023-03-11 16:06:49 +0900

    Spring Security + JWT Controller 변경

--- SpringLecture/myselectshop/java/com/sparta/myselectshop/controller/UserController.java
+++ SpringLecture/myselectshop/java/com/sparta/myselectshop/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.sparta.myselectshop.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.myselectshop.dto.LoginRequestDto;
 import com.sparta.myselectshop.dto.SignupRequestDto;
+import com.sparta.myselectshop.jwt.JwtUtil;
+import com.sparta.myselectshop.service.KakaoService;
 import com.sparta.myselectshop.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
@@ -16,13 +20,14 @@ import javax.servlet.http.HttpServletResponse;
 public class UserController {
 
     private final UserService userService;
+    private final KakaoService kakaoService;
 
     @GetMapping("/signup")
     public ModelAndView signupPage() {
         return new ModelAndView("signup");
     }
 
-    @GetMapping("/login")
+    @GetMapping("/login-page")
     public ModelAndView loginPage() {
         return new ModelAndView("login");
     }
@@ -30,7 +35,7 @@ public class UserController {
     @PostMapping("/signup")
     public String signup(SignupRequestDto signupRequestDto) {
         userService.signup(signupRequestDto);
-        return "redirect:/api/user/login";
+        return "redirect:/api/user/login-page";
     }
 
     @ResponseBody
@@ -38,6 +43,29 @@ public class UserController {
     public String login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
         userService.login(loginRequestDto, response);
         return "success";
+    }
+
+    @GetMapping("/forbidden")
+    public ModelAndView getForbidden() {
+        return new ModelAndView("forbidden");
+    }
+
+    @PostMapping("/forbidden")
+    public ModelAndView postForbidden() {
+        return new ModelAndView("forbidden");
+    }
+
+    @GetMapping("/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        // code: 카카오 서버로부터 받은 인가 코드
+        String createToken = kakaoService.kakaoLogin(code, response);
+
+        // Cookie 생성 및 직접 브라우저에 Set
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "redirect:/api/shop";
     }
 
 }
